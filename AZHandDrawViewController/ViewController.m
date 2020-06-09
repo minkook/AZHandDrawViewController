5dc91548a95648bbc6e1c259258bf4f64f0ab2e9
mkyoo <manguksmediawill@gmail.com>
2020-06-09 12:02:39 +0900

    init.

---
+++ AZHandDrawViewController/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  AZHandDrawViewController
+//
+//  Created by minkook yoo on 2020/06/09.
+//  Copyright © 2020 minkook yoo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    int a = 1;
+    
+}
+
+
+@end
